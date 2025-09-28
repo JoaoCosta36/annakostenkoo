1dd2eb5b360321e579ca16e9bbef31cb29cfc54a
João Costa <joaocostagr@outlook.pt>
2025-09-29 00:35:49 +0100

    changes

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Substitui 'annakostenkoo' pelo nome do teu repositório no GitHub
 export default defineConfig({
   base: '/annakostenkoo/',
   plugins: [react()],
